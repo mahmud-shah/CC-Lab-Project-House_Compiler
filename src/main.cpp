7ec26d2dc7f3ef4491a5d6a32a183f68f6abe1a2
Mahmud <me2010old@gmail.com>
2026-07-26 01:04:48 +0600

    Wire semantic analysis into compiler pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -9,7 +8,7 @@
 #include "minilang/ast_printer.hpp"
 #include "minilang/error_reporter.hpp"
 #include "minilang/lexer.hpp"
-#include "minilang/symbol_table.hpp"
+#include "minilang/semantic_analyzer.hpp"
 #include "minilang/symbol_table_printer.hpp"
 #include "parser.tab.hpp"
 
@@ -45,9 +44,9 @@ int runTokenDump(minilang::ErrorReporter& reporter) {
     int  kind;
     while ((kind = yylex()) != 0) {
         ++tokenCount;
+        std::string loc = std::to_string(minilang::ml_token_loc.line) + ":"
+                        + std::to_string(minilang::ml_token_loc.col);
 
-        std::string loc = std::to_string(minilang::ml_token_loc.line) + ":" +
-                          std::to_string(minilang::ml_token_loc.col);
         std::cout << std::left
                   << std::setw(10) << loc
                   << std::setw(16) << minilang::tokenName(kind)
@@ -57,11 +56,10 @@ int runTokenDump(minilang::ErrorReporter& reporter) {
             case TOK_INT_LIT:   std::cout << yylval.ival; break;
             case TOK_FLOAT_LIT: std::cout << yylval.fval; break;
             case TOK_KW_TRUE:
-            case TOK_KW_FALSE:  std::cout << (yylval.bval ? "true" : "false");
-                                break;
+            case TOK_KW_FALSE:  std::cout << (yylval.bval ? "true" : "false"); break;
             case TOK_IDENT:
                 std::cout << "\"" << yylval.sval << "\"";
-                std::free(yylval.sval); 
+                std::free(yylval.sval);
                 break;
             default: break;
         }
@@ -79,92 +77,64 @@ int runTokenDump(minilang::ErrorReporter& reporter) {
     return reporter.hasErrors() ? 1 : 0;
 }
 
-void buildSymbolTablePrePass(minilang::ASTNode* node,
-                             minilang::SymbolTable& table) {
-    if (!node) return;
-
-    using namespace minilang;
-
-    if (auto* p = dynamic_cast<ProgramNode*>(node)) {
-        for (StmtNode* s : p->statements) buildSymbolTablePrePass(s, table);
-    } else if (auto* b = dynamic_cast<BlockNode*>(node)) {
-        table.enterScope();
-        for (StmtNode* s : b->statements) buildSymbolTablePrePass(s, table);
-        table.exitScope();
-    } else if (auto* d = dynamic_cast<DeclarationNode*>(node)) {
-        Symbol sym;
-        sym.name         = d->name;
-        sym.type         = d->declType;
-        sym.scopeLevel   = table.currentLevel();
-        sym.declaredLine = d->loc.line;
-        sym.initialized  = false;
-        table.insert(sym);
-    } else if (auto* i = dynamic_cast<IfNode*>(node)) {
-        buildSymbolTablePrePass(i->thenBranch, table);
-        if (i->elseBranch) buildSymbolTablePrePass(i->elseBranch, table);
-    } else if (auto* w = dynamic_cast<WhileNode*>(node)) {
-        buildSymbolTablePrePass(w->body, table);
-    }
-}
-
 int runCompile(const Options& opts, minilang::ErrorReporter& reporter) {
+
     minilang::ProgramNode* ast = nullptr;
     yyparse(&ast);
 
     if (opts.ast && ast != nullptr) {
         minilang::ASTPrinter printer(std::cout);
         printer.print(*ast);
+        std::cout << "\n";
     }
 
-    minilang::SymbolTable table;
-    if (ast != nullptr) {
-        buildSymbolTablePrePass(ast, table);
+    minilang::SemanticAnalyzer semantic(reporter);
+    bool semanticOk = false;
+
+    if (ast != nullptr && !reporter.hasErrors()) {
+        semanticOk = semantic.analyze(*ast);
     }
+
 
     if (opts.symtab && ast != nullptr) {
         minilang::SymbolTablePrinter stp(std::cout);
-        stp.print(table);
+        stp.print(semantic.symbolTable());
     }
 
+    // ---- Diagnostics 
     int exitCode = 0;
     if (reporter.hasErrors()) {
         reporter.printAll(std::cerr);
         std::cerr << reporter.errorCount() << " error(s) found.\n";
         exitCode = 1;
     } else {
-        std::cout << "\nCompilation successful: lexical and syntax analysis "
-                  << "completed with no errors.\n";
+        std::cout << "Compilation successful.\n";
+        (void)semanticOk; 
     }
 
     delete ast;
     return exitCode;
 }
 
-}
+} 
 
 int main(int argc, char** argv) {
     Options opts;
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
-        if (arg == "--tokens") {
-            opts.tokens = true;
-        } else if (arg == "--ast") {
-            opts.ast = true;
-        } else if (arg == "--symtab") {
-            opts.symtab = true;
-        } else if (arg == "--help") {
-            printUsage(argv[0]);
-            return 0;
-        } else if (!arg.empty() && arg[0] == '-') {
+        if      (arg == "--tokens") opts.tokens = true;
+        else if (arg == "--ast")    opts.ast    = true;
+        else if (arg == "--symtab") opts.symtab = true;
+        else if (arg == "--help")   { printUsage(argv[0]); return 0; }
+        else if (!arg.empty() && arg[0] == '-') {
             std::cerr << "mcc: unknown option '" << arg << "'\n";
             printUsage(argv[0]);
             return 2;
         } else if (opts.sourcePath.empty()) {
             opts.sourcePath = arg;
         } else {
-            std::cerr << "mcc: multiple input files given ('" << opts.sourcePath
-                      << "' and '" << arg << "')\n";
+            std::cerr << "mcc: multiple input files\n";
             return 2;
         }
     }
